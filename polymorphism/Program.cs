fc415aca15c27138df1d1a6f7d32a642251bf4aa
tsidevhp001 <tsidevhp001@TSI-HP-001>
2022-02-14 17:37:20 +0530

    Add project files.

---
+++ polymorphism/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Inheritance
+{
+
+    // base class
+    class Animal
+    {
+        protected void eat()
+        {
+            Console.WriteLine("I can eat");
+        }
+    }
+
+    // derived class of Animal 
+    class Dog : Animal
+    {
+        static void Main(string[] args)
+        {
+
+            Dog labrador = new Dog();
+
+            // access protected method from base class
+            labrador.eat();
+
+            Console.ReadLine();
+        }
+    }
+}
